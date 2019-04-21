223e369d803e58962f70c8918474e88853c5bbf1
panoramix360 <lucasreis3000@gmail.com>
2019-04-21 02:52:21 -0300

    Adding jumping and moving

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpForce;
+    public LayerMask groundMask;
+
+    private bool isOnGround;
+    private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        CheckJump();
+
+        CheckOnGround();
+
+        // player movement
+        rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+    }
+
+    private void CheckJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    private void CheckOnGround()
+    {
+        Vector2 direction = Vector2.down;
+        float distance = 1f;
+
+        //Debug.DrawRay(transform.position, direction, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, distance, groundMask);
+        if (hit.collider != null)
+        {
+            isOnGround = true;
+        }
+        else
+        {
+            isOnGround = false;
+        }
     }
 }
